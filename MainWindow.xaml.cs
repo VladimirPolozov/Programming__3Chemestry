2677d7f492971817401e0c7f567036d64c46c7b7
Vladimir Polozov <z2005q.polozov@yandex.ru>
2024-09-23 20:14:44 +0700

    Created interface

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace FirstLab
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
     }
 }
